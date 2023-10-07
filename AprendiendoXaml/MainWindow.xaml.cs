39bd2e3f28debe6aa871ae4e048870567c99f480
Inma Gijón <igijoninf@gmail.com>
2023-10-07 09:34:08 +0200

    Cargar Xaml desde código.

--- AprendiendoXaml/MainWindow.xaml.cs
+++ AprendiendoXaml/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,12 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace AprendiendoXaml
 {
@@ -25,6 +28,23 @@ namespace AprendiendoXaml
             InitializeComponent();
         }
 
-        
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Create button
+            Button originalButton = new Button();
+            originalButton.Height = 50;
+            originalButton.Width = 100;
+            originalButton.Background = Brushes.AliceBlue;
+            originalButton.Content = "Click Me";
+
+            //Save the button to a string
+            string savedButton  = XamlWriter.Save(originalButton);
+
+            //Load the button
+            StringReader stringReader = new StringReader(savedButton);
+            XmlReader xmlReader = XmlReader.Create(stringReader);
+            Button readerLoadButton = (Button)XamlReader.Load(xmlReader);
+
+        }
     }
 }
