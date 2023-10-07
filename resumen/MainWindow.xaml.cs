ade0aa3db4e90119af4e965ad1b215128032d864
Inma Gijón <igijoninf@gmail.com>
2023-10-07 09:52:52 +0200

    Resumen de XAML y eventos. Pequeña práctica.

---
+++ resumen/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace resumen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAnswer_Click(object sender, RoutedEventArgs e)
+        {
+            this.Cursor = Cursors.Wait;
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(3));
+            Random random = new Random();
+            txtAnswer.Text = random.Next(0, 2) == 0 ? "No" : "Sí";
+            this.Cursor = null;
+        }
+    }
+}
